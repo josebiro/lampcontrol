7c23c69f5804cbbe64bd6bbf7e89c4c846997e3a
Joseph Bironas <josebiro@gmail.com>
2016-06-04 12:42:46 -0700

    Adding new buttons, removing hard coded values.

--- android-client/app/src/main/java/com/josebiro/lampcontrol/MainActivity.java
+++ android-client/app/src/main/java/com/josebiro/lampcontrol/MainActivity.java
@@ -1,10 +1,16 @@
 package com.josebiro.lampcontrol;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.ImageButton;
+import android.widget.SeekBar;
 import android.widget.Toast;
 
 import java.io.BufferedInputStream;
@@ -18,6 +24,9 @@ import java.net.ProtocolException;
 import java.net.URL;
 import java.net.HttpURLConnection;
 import java.nio.charset.StandardCharsets;
+
+import android.graphics.Color;
+import com.rtugeek.android.colorseekbar.ColorSeekBar;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -36,15 +45,41 @@ public class MainActivity extends AppCompatActivity {
      * See https://g.co/AppIndexing/AndroidStudio for more information.
      */
     private GoogleApiClient client;
+    private static MainActivity instance;
 
-    String lampServer = "http://192.168.86.91:8080";
-    String action;
-    Button btnLampOn;
-    Button btnLampOff;
+    private SharedPreferences settings;
+    private String serverUrl;
+
+    private String action;
+    private Button btnLampOn;
+    private Button btnLampOff;
+    private ImageButton btnSettings;
+
+    private Button btnRed;
+    private Button btnBlue;
+    private Button btnGreen;
+    private Button btnOrange;
+    private Button btnIndigo;
+    private Button btnWhite;
+    private Button btnYellow;
+    private Button btnViolet;
+    private Button btnPurple;
+
+    public String color;
+
+    public static MainActivity get() {
+        return instance;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        MainActivity.instance = this;
+
+        this.settings = getSharedPreferences("LampControlApp", Context.MODE_PRIVATE);
+
+        serverUrl = settings.getString("serverUrl", "http://192.168.86.91:8080");
+
         setContentView(R.layout.activity_main);
         // ATTENTION: This was auto-generated to implement the App Indexing API.
         // See https://g.co/AppIndexing/AndroidStudio for more information.
@@ -52,27 +87,105 @@ public class MainActivity extends AppCompatActivity {
 
         btnLampOn = (Button) findViewById(R.id.btn_lamp_on);
         btnLampOff = (Button) findViewById(R.id.btn_lamp_off);
+        btnSettings = (ImageButton) findViewById(R.id.btn_settings);
 
+        btnRed = (Button) findViewById(R.id.btn_red);
+        btnBlue = (Button) findViewById(R.id.btn_blue);
+        btnGreen = (Button) findViewById(R.id.btn_green);
+        btnOrange = (Button) findViewById(R.id.btn_orange);
+        btnIndigo = (Button) findViewById(R.id.btn_indigo);
+        btnWhite = (Button) findViewById(R.id.btn_white);
+        btnYellow = (Button) findViewById(R.id.btn_yellow);
+        btnViolet = (Button) findViewById(R.id.btn_violet);
+        btnPurple = (Button) findViewById(R.id.btn_purple);
+
+        final ColorSeekBar colorSeekBar = (ColorSeekBar) findViewById(R.id.colorSlider);
+        final CheckBox showAlphaCheckBox = (CheckBox) findViewById(R.id.checkBox);
+        final SeekBar barHeightSeekBar = (SeekBar) findViewById(R.id.seekBar);
+        final SeekBar thumbHeightSeekBar = (SeekBar) findViewById(R.id.seekBar2);
+        
         btnLampOn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                new LampWhiteOn().execute();
+                new SetColor().execute("white");
             }
         });
         btnLampOff.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                new LampOff().execute();
+                new SetColor().execute("off");
+            }
+        });
+        btnSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent settingsActivity = new Intent(v.getContext(),SettingsActivity.class);
+                startActivity(settingsActivity);
+            }
+        });
+        btnRed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new SetColor().execute("red");
+            }
+        });
+        btnBlue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new SetColor().execute("blue");
+            }
+        });
+        btnGreen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new SetColor().execute("green");
+            }
+        });
+        btnOrange.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new SetColor().execute("orange");
+            }
+        });
+        btnIndigo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new SetColor().execute("indigo");
+            }
+        });
+        btnWhite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new SetColor().execute("white");
+            }
+        });
+        btnYellow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new SetColor().execute("yellow");
+            }
+        });
+        btnViolet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new SetColor().execute("violet");
+            }
+        });
+        btnPurple.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new SetColor().execute("purple");
             }
         });
 
     }
 
-    private class LampWhiteOn extends AsyncTask<Void, Void, String> {
+    private class SetColor extends AsyncTask<String, Void, String> {
 
         @Override
-        protected String doInBackground(Void... params) {
-            Log.i("LampWhiteOn", "Entering LampWhiteOn method.");
+        protected String doInBackground(String... params) {
+            Log.i("SetColor", "Entering SetColor method.");
+            Log.i("SetColor", "Setting color to " + params[0]);
             HttpURLConnection c = null;
             BufferedReader r = null;
             String response;
@@ -80,19 +193,19 @@ public class MainActivity extends AppCompatActivity {
             // Send the lamp on command (white light)
             try {
                 action = new JSONObject();
-                action.put("colorname", "white");
-                Log.i("LampWhiteOn", action.toString());
+                action.put("colorname", params[0]);
+                Log.i("SetColor", action.toString());
             } catch (JSONException e) {
-                Log.e("LampOn", e.getLocalizedMessage());
+                Log.e("SetColor", e.getLocalizedMessage());
                 return null;
             }
             try {
                 byte[] postData = action.toString().getBytes("UTF-8");
                 int postDataLength = postData.length;
-                Log.i("LampOn", "Content Length is:" + postDataLength);
+                Log.i("SetColor", "Content Length is:" + postDataLength);
 
-                URL url = new URL(lampServer + "/setcolor");
-                Log.i("LampWhiteOn", url.toString());
+                URL url = new URL(serverUrl + "/setcolor");
+                Log.i("SetColor", url.toString());
 
                 c = (HttpURLConnection) url.openConnection();
                 c.setInstanceFollowRedirects(false);
@@ -101,6 +214,7 @@ public class MainActivity extends AppCompatActivity {
                 c.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
                 c.setRequestProperty("Content-Length", Integer.toString(postDataLength));
                 c.setUseCaches(false);
+                c.setConnectTimeout(500);
                 OutputStream os = c.getOutputStream();
                 os.write(postData);
                 os.close();
@@ -118,14 +232,14 @@ public class MainActivity extends AppCompatActivity {
                     buffer.append(line + "\n");
                 }
                 if (buffer.length() == 0) {
-                    Log.i("LampOn", "No data returned");
+                    Log.i("SetColor", "No data returned");
                 }
                 int status = c.getResponseCode();
                 response = buffer.toString();
-                Log.i("LampOn", "Response:" + status + response);
+                Log.i("SetColor", "Response:" + status + response);
             }
             catch (IOException e) {
-                Log.i("[ERROR]LampWhiteOn", e.getLocalizedMessage());
+                Log.i("[ERROR]SetColor", e.getLocalizedMessage());
                 return null;
             }
             finally {
@@ -136,7 +250,7 @@ public class MainActivity extends AppCompatActivity {
                     try {
                         r.close();
                     } catch (final IOException e) {
-                        Log.e("LampOn", "Error Closing Stream", e);
+                        Log.e("SetColor", "Error Closing Stream", e);
                     }
                 }
             }
@@ -144,80 +258,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private class LampOff extends AsyncTask<Void, Void, String> {
-        @Override
-        protected String doInBackground(Void... params) {
-            Log.i("LampOff", "Entering LampOff method.");
-            HttpURLConnection c = null;
-            BufferedReader r = null;
-            String response;
-            JSONObject action;
-            // Send the lamp on command (white light)
-            try {
-                action = new JSONObject();
-                action.put("colorname", "off");
-                Log.i("LampOff", action.toString());
-            } catch (JSONException e) {
-                Log.e("LampOff", e.getLocalizedMessage());
-                return null;
-            }
-            try {
-                byte[] postData = action.toString().getBytes("UTF-8");
-                int postDataLength = postData.length;
 
-                URL url = new URL(lampServer + "/setcolor");
-                Log.i("LampOff", url.toString());
-
-                c = (HttpURLConnection) url.openConnection();
-                c.setInstanceFollowRedirects(false);
-                c.setRequestMethod("POST");
-                c.setRequestProperty("X-Custom-Header", "lampcontrol");
-                c.setRequestProperty("Content-Type", "application/json");
-                c.setRequestProperty("charset", "utf=8");
-                c.setRequestProperty("Content-Length", Integer.toString(postDataLength));
-                c.setUseCaches(false);
-                OutputStream os = c.getOutputStream();
-                os.write(postData);
-                os.close();
-
-                InputStream input = new BufferedInputStream(c.getInputStream());
-                StringBuffer buffer = new StringBuffer();
-                if (input == null) {
-                    // nothing to do
-                    return null;
-                }
-                r = new BufferedReader(new InputStreamReader(input));
-
-                String line;
-                while ((line = r.readLine()) != null) {
-                    buffer.append(line + "\n");
-                }
-                if (buffer.length() == 0) {
-                    Log.i("LampOff", "No data returned");
-                }
-                int status = c.getResponseCode();
-                response = buffer.toString();
-                Log.i("LampOff", "Response:" + status + response);
-            }
-            catch (IOException e) {
-                Log.i("[ERROR]LampOff", e.getLocalizedMessage());
-                return null;
-            }
-            finally {
-                if (c != null) {
-                    c.disconnect();
-                }
-                if (r != null) {
-                    try {
-                        r.close();
-                    } catch (final IOException e) {
-                        Log.e("LampOff", "Error Closing Stream", e);
-                    }
-                }
-            }
-            return response;
-        }
-    }
 
     @Override
     public void onStart() {
